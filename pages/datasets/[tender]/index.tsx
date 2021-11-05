a762362d142f96128152ba99644cab8c707ceb7c
PixeledCode <ashoaib540918@gmail.com>
2021-11-05 17:59:19 +0530

    screen size fixes

--- pages/datasets/[tender]/index.tsx
+++ pages/datasets/[tender]/index.tsx
@@ -117,24 +117,25 @@ const Tender: React.FC<Props> = ({ data, loading }) => {
       <main className="tender">
         <MegaHeader data={headerData} />
         <div className="page-wrap container">
-          <section>
+          <section className="tender__heading">
             <h3 className="heading-w-line">Contract Details</h3>
+
+            <button className="button-primary">
+              <svg
+                width="10"
+                height="12"
+                viewBox="0 0 10 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.05967 4H6.99967V0.666667C6.99967 0.3 6.69967 0 6.33301 0H3.66634C3.29967 0 2.99967 0.3 2.99967 0.666667V4H1.93967C1.34634 4 1.04634 4.72 1.46634 5.14L4.52634 8.2C4.78634 8.46 5.20634 8.46 5.46634 8.2L8.52634 5.14C8.94634 4.72 8.65301 4 8.05967 4ZM0.333008 10.6667C0.333008 11.0333 0.633008 11.3333 0.999674 11.3333H8.99967C9.36634 11.3333 9.66634 11.0333 9.66634 10.6667C9.66634 10.3 9.36634 10 8.99967 10H0.999674C0.633008 10 0.333008 10.3 0.333008 10.6667Z"
+                  fill="white"
+                />
+              </svg>
+              Download
+            </button>
           </section>
-          <button className="button-primary">
-            <svg
-              width="10"
-              height="12"
-              viewBox="0 0 10 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8.05967 4H6.99967V0.666667C6.99967 0.3 6.69967 0 6.33301 0H3.66634C3.29967 0 2.99967 0.3 2.99967 0.666667V4H1.93967C1.34634 4 1.04634 4.72 1.46634 5.14L4.52634 8.2C4.78634 8.46 5.20634 8.46 5.46634 8.2L8.52634 5.14C8.94634 4.72 8.65301 4 8.05967 4ZM0.333008 10.6667C0.333008 11.0333 0.633008 11.3333 0.999674 11.3333H8.99967C9.36634 11.3333 9.66634 11.0333 9.66634 10.6667C9.66634 10.3 9.36634 10 8.99967 10H0.999674C0.633008 10 0.333008 10.3 0.333008 10.6667Z"
-                fill="white"
-              />
-            </svg>
-            Download
-          </button>
 
           <section className="tender__basic">
             <div>
