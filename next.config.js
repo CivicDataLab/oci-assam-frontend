1edfb7a882680a47ee3e2ff208928b7b8256f340
PixeledCode <ashoaib540918@gmail.com>
2022-02-10 14:33:00 +0530

    next config update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 const withTM = require('next-transpile-modules')(['echarts', 'zrender']);
 
-module.exports = withTM({
-  i18n: {
-    locales: ['en', 'fr', 'nl-NL', 'te'],
-    defaultLocale: 'en',
-  },
-  publicRuntimeConfig: {
-    DMS: 'http://15.206.122.72/',
-    CMS: 'https://oddk.home.blog',
-  },
-});
+module.exports = withTM();
