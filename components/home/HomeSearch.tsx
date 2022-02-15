13333005c9a91b9d4cf125d2de5ddf76c77edcce
Abhinav Kumar <a.kumar2102@gmail.com>
2022-02-15 08:04:21 +0530

    Linked - home page icons

--- components/home/HomeSearch.tsx
+++ components/home/HomeSearch.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import Search from 'components/datasets/Search';
+import Link from 'next/link';
 
 const HomeSearch = () => {
   const router = useRouter();
@@ -16,16 +17,22 @@ const HomeSearch = () => {
       <Search newSearch={handleRouteChange} />
       <div className="home-search__icons">
         <div className="home-search__category">
-          <img src="/assets/icons/health.svg" alt="" />
-          <h3>Health Related Tenders</h3>
+          <a href="/datasets?fq=&q=health">
+          	<img src="/assets/icons/health.svg" alt="" />
+          	<h3>Health Related Tenders</h3>
+          </a>
         </div>
         <div className="home-search__category">
-          <img src="/assets/icons/water.svg" alt="" />
-          <h3>Water and Sanitation</h3>
+	 <a href="/datasets?fq=&q=water">
+		  <img src="/assets/icons/water.svg" alt="" />
+		  <h3>Water and Sanitation</h3>
+         </a>
         </div>
         <div className="home-search__category">
-          <img src="/assets/icons/flood.svg" alt="" />
-          <h3>Flood Management and Relief</h3>
+	 <a href="/datasets?fq=&q=flood">
+		  <img src="/assets/icons/flood.svg" alt="" />
+		  <h3>Flood Management and Relief</h3>
+         </a>
         </div>
       </div>
       <a href="/datasets" className="btn-primary home-search__button">
